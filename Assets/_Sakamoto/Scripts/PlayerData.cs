6a9269198e07b7df1228ae46885c529a1260acd1
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-10-06 14:04:18 +0900

    [add]jump

--- Assets/_Sakamoto/Scripts/PlayerData.cs
+++ Assets/_Sakamoto/Scripts/PlayerData.cs
@@ -4,13 +4,19 @@ public class PlayerData : MonoBehaviour
 {
     [Header("Reference")]
     [SerializeField] private Transform _cameraForward;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _playerHeight;
 
     [Header("Move")]
     [SerializeField] private float _walkSpeed;
     [SerializeField] private float _sprintSpeed;
+
+    [Header("Jump")]
     [SerializeField] private float _jumpForce;
     
     public Transform CameraForward => _cameraForward;
+    public LayerMask GroundLayer => _groundLayer;
+    public float PlayerHeight => _playerHeight;
     public float WalkSpeed => _walkSpeed;
 
     public float SprintSpeed => _sprintSpeed;
