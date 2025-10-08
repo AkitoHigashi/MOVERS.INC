9ec3435e5a6ec40a1d30d79e5e9b77dd44dd8961
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2025-10-08 19:04:17 +0900

    [update]トラップの範囲内にはいったら動きだし範囲外に行くと止まるようにした

--- Assets/_Kabotya/Trap/TrapCS/FurikoTrap.cs
+++ Assets/_Kabotya/Trap/TrapCS/FurikoTrap.cs
@@ -7,20 +7,46 @@ public class FurikoTrap : MonoBehaviour
     [Tooltip("下げたらスピードが上がる")]
     [SerializeField] private float _duration = 1f;
     [Tooltip("-1ならループ、その他の整数を入れるとその回数ループ")]
-    [SerializeField] private int _loopNumber　= -1;
+    [SerializeField] private int _loopNumber = -1;
 
     [Tooltip("何度回転するか")]
     private float _swingAngle = 180f;
+
+    private Tween _furikoTween;
 
     private void Start()
     {
         RotateFuriko();
     }
 
-    private void RotateFuriko() 
+    private void Update()
     {
-        transform.DORotate(new Vector3(0, 0, _swingAngle), _duration)
-         .SetLoops(_loopNumber, LoopType.Yoyo)
-         .SetEase(Ease.InOutQuad);
+        FurikoCheck();
+    }
+
+    private void FurikoCheck()
+    {
+        if (TrapRange.Instance._deactivateWhenExit)
+        {
+            // プレイヤーが範囲外にいる場合、トラップを停止
+            if (_furikoTween != null && _furikoTween.IsPlaying())
+            {
+                _furikoTween.Pause();
+            }
+        }
+        else
+        {
+            // プレイヤーが範囲内にいる場合、トラップを再開
+            if (_furikoTween != null && !_furikoTween.IsPlaying())
+            {
+                _furikoTween.Play();
+            }
+        }
+    }
+    private void RotateFuriko()
+    {
+        _furikoTween = transform.DORotate(new Vector3(0, 0, _swingAngle), _duration)
+        .SetLoops(_loopNumber, LoopType.Yoyo)
+        .SetEase(Ease.InOutQuad);
     }
 }
---
+++ Assets/_Kabotya/Trap/TrapCS/TrapRange.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TrapRange : MonoBehaviour
+{
+    public static TrapRange Instance;
+    [SerializeField] private GameObject _trap;
+    [SerializeField] public bool _deactivateWhenExit = true;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _deactivateWhenExit = false;
+            Debug.Log("トラップを開始");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _deactivateWhenExit = true;
+            Debug.Log("トラップをストップ");
+        }
+    }
+}
