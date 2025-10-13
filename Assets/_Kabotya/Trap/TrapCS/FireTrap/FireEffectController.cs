e3a7a06001a017fabb92464fadf27fc21c34893f
KINOKO\nubob <Ki28ryu@icloud.com>
2025-10-13 17:02:18 +0900

    [add]火炎放射器の当たり判定を伸ばす動作を作った

---
+++ Assets/_Kabotya/Trap/TrapCS/FireTrap/FireEffectController.cs
@@ -0,0 +1,41 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FireEffectController : MonoBehaviour
+{
+    [SerializeField]private BoxCollider _box;
+    [SerializeField]private float _maxHit = 5;
+    private float _sizeNum = 1f;
+    private float _centerNum = 1f;
+
+    private void Start()
+    {
+        _box = GetComponent<BoxCollider>();
+        _box.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        Firing();
+    }
+
+    //発砲
+    private void Firing() 
+    {
+        Flame();
+    }
+
+    private void Flame()
+    {
+        if (_box.size.z < _maxHit)
+        {
+            Vector3 newSize = _box.size;
+            Vector3 newCenter = _box.center;
+            newSize.z += _sizeNum * Time.deltaTime;
+            newCenter.z += _centerNum * Time.deltaTime / 2;
+            _box.size = newSize;
+            _box.center = newCenter;
+        }
+    }
+}
