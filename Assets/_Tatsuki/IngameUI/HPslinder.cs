f74710b60de4c925cd6c5e5e8c96767c06a9d4bf
kenkatou354 <kenkatou354@gmail.com>
2025-10-15 15:39:52 +0900

    [Add]hp,sutaminaのシリンダーを変更できるように、仮通知システムを追加

---
+++ Assets/_Tatsuki/IngameUI/HPslinder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class HPslinder : MonoBehaviour
+{
+    [SerializeField]  private Slider hpslider;
+    [SerializeField] private Slider hsliders;
+    [SerializeField] private InvokeSystem InvokeSystem;
+
+    private void OnEnable()
+    {
+        InvokeSystem.GetHp += hpSetSlider;
+        InvokeSystem.GetRunGauge += runSetSlider;
+    }
+
+    private void OnDisable()
+    {
+        InvokeSystem.GetHp -= hpSetSlider;
+        InvokeSystem.GetRunGauge -= runSetSlider;
+    }
+
+    public void hpSetSlider(float sliderValue)
+    {
+        hpslider.value += sliderValue;
+    }
+    public void runSetSlider(float sliderValue)
+    {
+        hsliders.value += sliderValue;
+    }
+
+
+
+
+}
---
+++ Assets/_Tatsuki/IngameUI/InvokeSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class InvokeSystem : MonoBehaviour
+{
+    public event Action<float> GetHp;
+    public event Action<float> GetRunGauge;
+
+    float time = 0f;
+    float damage = 0.1f;
+    
+    private void Update()
+    {
+        
+           time += Time.deltaTime;
+        if (time > 1f)
+        {
+            GetHp?.Invoke(-damage);
+            GetRunGauge?.Invoke(-damage);
+            time = 0f;
+        }
+    }
+}
