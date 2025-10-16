92b1f371121798d3ca40649b969cc94778618939
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-10-16 12:24:26 +0900

    [add]仮のプレイヤーHp

---
+++ Assets/_Miyamoto/Scripts/DemoPlayerHP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MiyamotoDemoPlayer
+{
+    public class DemoPlayerHP : MonoBehaviour
+    {
+        [SerializeField]
+        private float _Hp;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("MonsterWeapon"))
+            {
+                var weapon = other.GetComponent<MonsterWeapon>();
+                _Hp -= weapon.Power;
+            }
+        }
+    }
+}
