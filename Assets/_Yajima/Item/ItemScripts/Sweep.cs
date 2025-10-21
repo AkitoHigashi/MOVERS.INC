2b742ecf1472957e71f4440c5fb8e01460242c4d
KNTy1018 <soramametaro1018@gmail.com>
2025-10-21 15:40:39 +0900

    [update]とりあえず関数は作った

---
+++ Assets/_Yajima/Item/ItemScripts/Sweep.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 横に薙ぎ払うものにアタッチするクラス
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class Sweep : ItemBase
+{
+    [SerializeField] string _animationName;
+    [SerializeField] float _power;
+    Animator _anim;
+    /// <summary>2度振り禁止フラグ</summary>
+    bool _isPlaying;
+
+    protected override void Init()
+    {
+        base.Init();
+        _anim = GetComponent<Animator>();
+    }
+
+    public override void ItemActivate()
+    {
+        if (!_isPlaying)
+        {
+            _isPlaying = true;
+            _anim.Play(_animationName);
+        }
+    }
+
+    /// <summary>
+    /// アニメーションイベントで呼び出す
+    /// </summary>
+    public void ActivateEnd()
+    {
+        _isPlaying = false;
+    }
+}
--- Assets/_Yajima/Item/ItemScripts/Sword.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Sword : ItemBase
-{
-    public override void ItemActivate()
-    {
-        
-    }
-}
