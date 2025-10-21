eb56834b362ea46faa6bd7d2915e66d1225ee0a0
KNTy1018 <soramametaro1018@gmail.com>
2025-10-21 13:50:55 +0900

    [add]戦闘過程を司るクラスとインターフェースを作成途中

---
+++ Assets/_Yajima/BattleProcess.cs
@@ -0,0 +1,15 @@
+﻿using ProcessInterface;
+
+/// <summary>戦闘過程を司るクラス</summary>
+public static class BattleProcess
+{
+    /// <summary>
+    /// ダメージ計算を行うクラス
+    /// </summary>
+    /// <param name="attack">攻撃側</param>
+    /// <param name="hit">ダメージを受ける側</param>
+    public static void DamageCalcurate(IAttackable attack,IHittable hit)
+    {
+        hit.TakeDamage(attack.Power);
+    }
+}
---
+++ Assets/_Yajima/ProcessInterface.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcessInterface
+{
+    /// <summary>攻撃を食らうものが継承するインターフェース</summary>
+    public interface IHittable
+    {
+        /// <summary>
+        /// ダメージを受ける関数
+        /// </summary>
+        /// <param name="power">ダメージ</param>
+        public void TakeDamage(float power);
+    }
+
+    /// <summary>攻撃するものが継承するインターフェース</summary>
+    public interface IAttackable
+    {
+        /// <summary>
+        /// 攻撃力を受け取るプロパティ
+        /// </summary>
+        public float Power {  get; }
+    }
+}
