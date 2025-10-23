8549f4949ffb7f5febc52798fde403beeff2f373
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2025-10-23 19:55:43 +0900

    [add]サークルトラップの作成

---
+++ Assets/_Kabotya/Trap/TrapCS/BladeTrap/BladeTrap2/CirculBladeTrap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CirculBladeTrap : TrapBase
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private float _circulBladeTrapXSpeed = 10f;
+
+    void Update()
+    {
+        _gameObject.transform.Rotate(0, _circulBladeTrapXSpeed, 0);
+    }
+}
