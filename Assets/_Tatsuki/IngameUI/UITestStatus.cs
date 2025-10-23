6af87df5b6287a215f282916ab37ac789307ee95
kenkatou354 <kenkatou354@gmail.com>
2025-10-23 18:28:34 +0900

    [Add]プレイヤーのヘルス情報を持つクラスを追加

--- Assets/_Tatsuki/IngameUI/UITestStatus.cs
+++ Assets/_Tatsuki/IngameUI/UITestStatus.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class UITestStatus : MonoBehaviour
@@ -6,12 +7,23 @@ public class UITestStatus : MonoBehaviour
     private LuggageGenerator _luggageGenerator;
     public static int maxItem = 10;
     public static int UImaxHp = 100;
+    public static int CurrentHp = 100;
     public static int UImaxRunGauge = 100;
+    private PlayerHealth _playerHealth;
 
-    private void Start()
+    private void Awake()
     {
+        _playerHealth = FindAnyObjectByType<PlayerHealth>();
         _luggageGenerator = FindAnyObjectByType<LuggageGenerator>();
         maxItem = _luggageGenerator.GetTargetValue();
         Debug.Log(maxItem);
+        UImaxHp = (int)_playerHealth.PlayerHP;
+        CurrentHp = (int)_playerHealth.CurrentHP;
+
+    }
+
+    public void SetHealth()
+    {
+        UImaxHp  = (int)_playerHealth.CurrentHP;
     }
 }
