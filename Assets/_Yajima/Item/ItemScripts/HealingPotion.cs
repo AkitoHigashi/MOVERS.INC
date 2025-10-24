065c20c3f17ee8f4306a76c0ee96c42a7720d5bb
KNTy1018 <soramametaro1018@gmail.com>
2025-10-24 23:03:04 +0900

    [add]ポーションのスクリプト追加

---
+++ Assets/_Yajima/Item/ItemScripts/HealingPotion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HealingPotion : ItemBase
+{
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+    public override void ItemActivate()
+    {
+
+    }
+}
---
+++ Assets/_Yajima/Item/ItemScripts/MusclePotion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MusclePotion : ItemBase
+{
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+    public override void ItemActivate()
+    {
+
+    }
+}
