828c070ba166f2c654ce7533d034d6a526651586
KNTy1018 <soramametaro1018@gmail.com>
2025-10-24 23:30:11 +0900

    [add]ツールキットスクリプト作成

---
+++ Assets/_Yajima/Item/ItemScripts/ToolKit.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class ToolKit : ItemBase
+{
+    [SerializeField, Tooltip("効果発動時間")] float _effectiveTime = 1;
+    BoxCollider _bc;
+    Coroutine _coroutine;
+
+    protected override void Init()
+    {
+        base.Init();
+        _bc = GetComponent<BoxCollider>();
+        _bc.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        if( _coroutine != null)
+        {
+            StopCoroutine( _coroutine );
+            _coroutine = null;
+        }
+    }
+
+    public override void ItemActivate()
+    {
+        if (_coroutine == null)
+        {
+            _coroutine = StartCoroutine(CoolTimeCoroutine());
+        }
+    }
+
+    IEnumerator CoolTimeCoroutine()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        _bc.enabled = true;
+        yield return new WaitForSeconds(_effectiveTime);
+        _bc.enabled = false;
+        _coroutine = null;
+        yield break;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "TrapBase")
+        {
+            TrapDestroy(other.gameObject);
+        }
+    }
+
+    void TrapDestroy(GameObject trap)
+    {
+        Destroy(trap);
+    }
+}
