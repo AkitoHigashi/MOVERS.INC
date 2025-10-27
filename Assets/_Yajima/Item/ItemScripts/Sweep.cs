6564b731d631d6e1bf96063e9b4883328c64b684
KNTy1018 <soramametaro1018@gmail.com>
2025-10-27 13:35:20 +0900

    [update]アニメーションの変な挙動を修正した

--- Assets/_Yajima/Item/ItemScripts/Sweep.cs
+++ Assets/_Yajima/Item/ItemScripts/Sweep.cs
@@ -6,13 +6,11 @@
 public class Sweep : ItemBase
 {
     [SerializeField] GameObject _sweep;
-    private GameObject _cameraPos;//カメラ
 
     protected override void Init()
     {
         base.Init();
         _sweep.SetActive(false);
-        _cameraPos = GameObject.Find("Camera");
 
     }
 
@@ -22,9 +20,9 @@ public class Sweep : ItemBase
         if (!_sweep.activeSelf)
         {
             //プレイヤーの子にする想定
-            _sweep.transform.SetParent(_cameraPos.transform);
-            _sweep.transform.position = _cameraPos.transform.position;
-            _sweep.transform.rotation = _cameraPos.transform.rotation;
+            _sweep.transform.SetParent(_cameraTrans);
+            _sweep.transform.position = _cameraTrans.position;
+            _sweep.transform.rotation = _cameraTrans.rotation;
             _sweep.SetActive(true);
             gameObject.SetActive(false);
         }
