0c02cf9a3d01964ddcfeb498a8b29ccf50818479
hiaki0617 <hiaki0617@gmail.com>
2025-10-27 13:16:48 +0900

    [add,change]張さんのモンスタースポーンスクリプトと矢嶋と坂本のアイテムuseを修正

--- Assets/_Sakamoto/Scripts/PlayerCarry.cs
+++ Assets/_Sakamoto/Scripts/PlayerCarry.cs
@@ -76,6 +76,7 @@ public class PlayerCarry : MonoBehaviour, IStartSetVariables
                 Physics.IgnoreCollision(_playerCollider, _luggageData.LuggageCollider, false);
             _luggageData.LuggageRb.isKinematic = false;
             _luggageData.LuggageGameObject.transform.SetParent(null);
+            _luggageData.LuggageGameObject = null;
             _luggageData.LuggageScript = null;
             _isCarrying = false;
         }
