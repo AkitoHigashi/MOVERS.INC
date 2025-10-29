bb5ebcc2ecd71204081ffc0ebb55ede6551cfa5c
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-10-29 16:03:26 +0900

    [update]視野の拡大倍率を変数にしてマジックナンバー回避

--- Assets/_Miyamoto/Scripts/MonsterBase.cs
+++ Assets/_Miyamoto/Scripts/MonsterBase.cs
@@ -46,6 +46,8 @@ public abstract class MonsterBase : MonoBehaviour
     private GameObject _deadPrefab;
     [SerializeField, Header("ターゲットにするタグ")]
     private List<string> _targetTags;
+    [SerializeField, Header("視野の拡大倍率")]
+    private float _fovMagnification;
 
     #region ステータス
     protected float _monsterHp;
@@ -209,7 +211,10 @@ public abstract class MonsterBase : MonoBehaviour
     /// <returns></returns>
     private IEnumerator ChangeDestination()
     {
-        if (_hasSeen) yield break;
+        if (_hasSeen)
+        {
+            yield break;
+        }
 
         _navMeshAgent.isStopped = true;
         _lookAround = true;
@@ -321,8 +326,8 @@ public abstract class MonsterBase : MonoBehaviour
     protected virtual void FirstSeeing()
     {
         Debug.Log("初めて見えたFirstSeeing呼び出し");
-        _navMeshAgent.angularSpeed *= 2;
-        _fov *= 2;
+        _navMeshAgent.angularSpeed *= _fovMagnification;
+        _fov *= _fovMagnification;
         _hasSeen = true;
     }
     /// <summary>
@@ -331,8 +336,8 @@ public abstract class MonsterBase : MonoBehaviour
     protected void ResetVision()
     {
         Debug.Log("視野角リセットResetVision呼び出し");
-        _navMeshAgent.angularSpeed /= 2;
-        _fov /= 2;
+        _navMeshAgent.angularSpeed /= _fovMagnification;
+        _fov /= _fovMagnification;
         _hasSeen = false;
     }
     /// <summary>
