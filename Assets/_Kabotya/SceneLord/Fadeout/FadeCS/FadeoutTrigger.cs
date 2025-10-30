8a2fad413f1a5b183bc9e42294034f0aa4832d84
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2025-10-30 20:26:48 +0900

    [add]タイトル画面のもとを作成

--- Assets/_Kabotya/SceneLord/Fadeout/FadeCS/FadeoutTrigger.cs
+++ Assets/_Kabotya/SceneLord/Fadeout/FadeCS/FadeoutTrigger.cs
@@ -1,12 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Playables;
 
 public class FadeoutTrigger : MonoBehaviour
 {
     [SerializeField] private PlayableDirector _fadeTimeline;
+    [SerializeField] private GameObject targetObject;
+
+    public void Start()
+    {
+        targetObject.SetActive(false);
+    }
 
     public void OnFadeButtonPressed()
     {
+        targetObject.SetActive(true);
         _fadeTimeline.Play();
     }
 }
