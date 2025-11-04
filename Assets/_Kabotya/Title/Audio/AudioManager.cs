58213e1c78776df9ce030ad84e94fc3e321fa910
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2025-11-04 16:52:45 +0900

    [add]いろいろつくった、フェードアウトのプレハブも作った

---
+++ Assets/_Kabotya/Title/Audio/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+    private void Update()
+    {
+        float value = _slider.value;
+        Debug.Log("Slider Value: " + value);
+    }
+}
--- Assets/_Kabotya/Title/SceneLord/Fadeout/FadeCS/FadeoutTrigger.cs
+++ Assets/_Kabotya/Title/SceneLord/Fadeout/FadeCS/FadeoutTrigger.cs
@@ -1,19 +1,13 @@
 ﻿using UnityEngine;
-using UnityEngine.Playables;
 
 public class FadeoutTrigger : MonoBehaviour
 {
-    [SerializeField] private PlayableDirector _fadeTimeline;
+    [SerializeField] private Animator _fadeAnimator;
     [SerializeField] private GameObject targetObject;
-
-    public void Start()
-    {
-        targetObject.SetActive(false);
-    }
 
     public void OnFadeButtonPressed()
     {
         targetObject.SetActive(true);
-        _fadeTimeline.Play();
+        _fadeAnimator.SetTrigger("Fade");
     }
 }
