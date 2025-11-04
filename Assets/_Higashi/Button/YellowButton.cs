689296c9fdc4e850b6ca505e5ea37714f89dcf58
hiaki0617 <hiaki0617@gmail.com>
2025-11-04 16:42:20 +0900

    [add]インゲームのリザルト画面を表示するyellowButtonを作成（まだ途中） CollectionButtonをは消した。

---
+++ Assets/_Higashi/Button/YellowButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ロビーに帰還をするためのボタンクラス
+/// </summary>
+public class YellowButton : InteractBase
+{
+    [SerializeField, Header("ロードするシーンの名前")] private string _sceneName;
+    private Animator _animator;
+    public override void Interact()
+    {
+        _animator.SetTrigger("push");
+        SceneLordManager.Instance.Scnenlode(_sceneName);
+    }
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+    public override void DemolishedLuggage()
+    {
+        Debug.Log("なにも実装されていません");
+    }
+    public override void PutLuggage(Collision collision)
+    {
+        Debug.Log("なにも実装されていません");
+    }
+}
